5b7d24265f6ce3e662ec2ef016c7d0213d060a47
Nik <nikita.pitushkin31@mail.com>
2025-09-20 14:59:21 +0300

    The errors have been fixed.

--- app/main.py
+++ app/main.py
@@ -4,51 +4,44 @@ class Car:
         self.clean_mark = clean_mark
         self.brand = brand
 
+
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int ) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int, average_rating: float,
+        count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car: Car):
+    def calculate_washing_price(self, car: Car) -> float:
         price = (car.comfort_class
                  * (self.clean_power - car.clean_mark)
                  * self.average_rating
                  / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car: Car):
+    def wash_single_car(self, car: Car) -> float:
         if self.clean_power > car.clean_mark:
             price = self.calculate_washing_price(car)
-            car.clean_mark =  self.clean_power
+            car.clean_mark = self.clean_power
             return price
         else:
             return 0
 
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
             income += self.wash_single_car(car)
         return income
 
-    def rate_service(self, rate: float):
+    def rate_service(self, rate: float) -> None:
         self.average_rating = round(
-                (self.average_rating * self.count_of_ratings + rate)
-                / (self.count_of_ratings + 1),
-                1
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1),
+            1
         )
         self.count_of_ratings += 1
-
-
-
-
-
-
-
-
-
-
-
-
-
